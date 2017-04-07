d0cffacf089b9a612e40403cda2ceea58e5cb3eb
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-04-07 17:25:37 +0300

    Extracted inline styles to a separate styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -12,13 +13,14 @@ const config = {
         use: 'babel-loader',
         test: /\.js$/,
       }, {
-        use: [
-          'style-loader', 'css-loader',
-        ],
+        loader: ExtractTextPlugin.extract({ loader: 'css-loader' }),
         test: /\.css$/,
-      }
-    ]
-  }
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
 };
 
 module.exports = config;
